cc9992f51e00239976c28d0a5d9a87fa1ff605db
vinayyyyyyy <vinaykarthiktripuraneni@gmail.com>
2025-09-06 19:56:08 +0530

    Delete library-management-system-main directory

--- library-management-system-main/src/app/config/index.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-
-export default {
-  port: process.env.PORT,
-  mongodbUri: process.env.MONGODB_URI,
-};
--- library-management-system-main/src/app/modules/borrow/borrow.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { borrowBook, getBorrowBooks } from "./borrow.controller";
-
-const borrowRouter = Router();
-
-borrowRouter.post("/", borrowBook);
-borrowRouter.get("/", getBorrowBooks);
-
-export default borrowRouter;
